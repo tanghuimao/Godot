b81045b03ea79aa4fcb273e76151e06b6ec6ae98
THM <40234887+tanghuimao@users.noreply.github.com>
2024-04-28 23:39:57 +0800

    -fix 修改敌人掉落

--- ClassicRoguelikeCourse/Resources/CharacterData/EnemyData/EnemyData.cs
+++ ClassicRoguelikeCourse/Resources/CharacterData/EnemyData/EnemyData.cs
@@ -13,5 +13,18 @@ public partial class EnemyData : CharacterData
     public float DeathDropExperience;
     //死亡掉落物品字典 Dictionary使用godot集合中的字典
     [Export]
-    public Dictionary<PackedScene, float> DeathDropPickableObjects = new ();
+    public Dictionary<PackedScene, float> DeathDropPickableObjects = new ()
+    {
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Items/SmallPotion.tscn"), 0.5f}, //小血瓶
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Items/BigPotion.tscn"), 0.5f}, //大血瓶
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Items/SightBall.tscn"), 0.5f}, //视力宝珠
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Items/MapScroll.tscn"), 0.5f}, //地图卷轴
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Items/ReviveCross.tscn"), 0.5f}, //复活十字
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Equipments/Sword.tscn"), 0.5f}, //剑
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Equipments/Mace.tscn"), 0.5f}, //锤
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Equipments/Shield.tscn"), 0.5f}, //盾
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Equipments/Armour.tscn"), 0.5f}, //盔甲
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Equipments/Ring.tscn"), 0.5f}, //戒指
+        {GD.Load<PackedScene>("res://Entities/PickableObjects/Equipments/Necklace.tscn"), 0.5f}, //项链
+    };   
 }
